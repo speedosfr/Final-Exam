43327e59666b8d9c2d22197e670687dbb2fefb3d
Laurent Lecomte <dynaouest@gmail.com>
2017-12-10 12:05:42 +0100

    Correction ligne10 + indentation

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -1,13 +1,13 @@
 describe('Test des parametres de la fonction', function() {
 	
 	it('la fonction doit prendre 3 parametres', function() {
-		expect(convertisseur('USD')).toBe(null);
-		expect(convertisseur('USD', 2)).toBe(null);
-		expect(convertisseur('USD', 2, 'EUR')).not.toBe(null);
+	expect(convertisseur('USD')).toBe(null);
+	expect(convertisseur('USD', 2)).toBe(null);
+	expect(convertisseur('USD', 2, 'EUR')).not.toBe(null);
     });
     
   it('les 3 parametres doivent etre du bon type', function() {
-		expect(convertisseur('abc', 5,'abc'))..not.toBe(null);
+	expect(convertisseur('abc', 5,'abc')).not.toBe(null);
         expect(convertisseur(5,25,'abc')).toBe(null);
         expect(convertisseur('acd',17,25)).toBe(null);
         expect(convertisseur('acd','cbh','vgh')).toBe(null);
@@ -19,7 +19,7 @@ describe('Test des parametres de la fonction', function() {
 describe('Test resultats de la fonction', function() {
 	
 	it('doit convertir des USD dans les autres monnaies', function() {
-	    	expect(convertisseur('USD', 1, 'NZD')).toEqual(1.45);
+	expect(convertisseur('USD', 1, 'NZD')).toEqual(1.45);
         expect(convertisseur('USD', 1, 'KRW')).toEqual(1086);
         expect(convertisseur('USD', 1, 'EUR')).toEqual(0.84);
     });
@@ -31,19 +31,19 @@ describe('Test resultats de la fonction', function() {
     });
 
     it('doit convertir des KRW dans les autres monnaies', function() {
-		expect(convertisseur('NZD', 1, 'USD')).not.toBe(null);
+	expect(convertisseur('NZD', 1, 'USD')).not.toBe(null);
         expect(convertisseur('NZD', 1, 'KRW')).not.toBe(null);
         expect(convertisseur('NZD', 1, 'USD')).not.toBe(null);
     });
 
     it('doit convertir des NZD dans les autres monnaies', function() {
-		expect(convertisseur('KRW', 1, 'EUR')).not.toBe(null);
+	expect(convertisseur('KRW', 1, 'EUR')).not.toBe(null);
         expect(convertisseur('KRW', 1, 'KRW')).not.toBe(null);
         expect(convertisseur('KRW', 1, 'USD')).not.toBe(null);
     });
 
      it('ne doit pas convertir des mauvais montants', function() {
-		expect(convertisseur('USD',2,'EUR')).not.toBe(null);
+	expect(convertisseur('USD',2,'EUR')).not.toBe(null);
         expect(convertisseur('USD',0,'EUR')).toBe(null);
         expect(convertisseur('USD',,'EUR')).toBe(null);
         expect(convertisseur('USD',-5,'EUR')).toBe(null);
