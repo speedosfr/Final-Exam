caad8370ed18f32c8825930f6946ac0a5f13c880
Jcoub1 <jerome.coubronne@gmail.com>
2017-12-11 13:51:58 +0100

    modification test

--- spec/convert.spec.js
+++ spec/convert.spec.js
@@ -7,7 +7,7 @@ describe('Test des parametres de la fonction', function() {
     });
     
   it('les 3 parametres doivent etre du bon type', function() {
-	//expect(convertisseur(5,25,'abc')).toBe(null);
+	    expect(convertisseur(5,25,'abc')).toBe(null);
         expect(convertisseur('acd',17,25)).toBe(null);
         expect(convertisseur('acd','cbh','vgh')).toBe(null);
         expect(convertisseur(1,2,3)).toBe(null);
@@ -18,27 +18,27 @@ describe('Test des parametres de la fonction', function() {
 describe('Test resultats de la fonction', function() {
 	
 	it('doit convertir des USD dans les autres monnaies', function() {
-	expect(convertisseur('USD', 1, 'NZD')).toEqual(1.45);
+	    expect(convertisseur('USD', 1, 'NZD')).toEqual(1.45);
         expect(convertisseur('USD', 1, 'KRW')).toEqual(1086);
         expect(convertisseur('USD', 1, 'EUR')).toEqual(0.84);
     });
     
     it('doit convertir des EUR dans les autres monnaies', function() {
-        expect(convertisseur('EUR', 1, 'NZD')).not.toBe(null);
-        expect(convertisseur('EUR', 1, 'KRW')).not.toBe(null);
-        expect(convertisseur('EUR', 1, 'USD')).not.toBe(null);
-    });
-
-    it('doit convertir des KRW dans les autres monnaies', function() {
-	expect(convertisseur('NZD', 1, 'USD')).not.toBe(null);
-        expect(convertisseur('NZD', 1, 'KRW')).not.toBe(null);
-        expect(convertisseur('NZD', 1, 'USD')).not.toBe(null);
+        expect(convertisseur('EUR', 1, 'NZD')).toEqual(1.682);
+        expect(convertisseur('EUR', 1, 'KRW')).toEqual(1259.76);
+        expect(convertisseur('EUR', 0.86, 'USD')).toEqual(1);
     });
 
     it('doit convertir des NZD dans les autres monnaies', function() {
-	expect(convertisseur('KRW', 1, 'EUR')).not.toBe(null);
-        expect(convertisseur('KRW', 1, 'KRW')).not.toBe(null);
-        expect(convertisseur('KRW', 1, 'USD')).not.toBe(null);
+	    expect(convertisseur('NZD', 1.45, 'USD')).toEqual(1);
+        expect(convertisseur('NZD', 1, 'KRW')).toEqual(738.48);
+        expect(convertisseur('NZD', 1, 'EUR')).toEqual(0.57);
+    });
+
+    it('doit convertir des KRW dans les autres monnaies', function() {
+	    expect(convertisseur('KRW', 1259.76, 'EUR')).toEqual(1);
+        expect(convertisseur('KRW', 738.48, 'NZD')).toEqual(1);
+        expect(convertisseur('KRW', 1086, 'USD')).toEqual(1);
     });
 
      it('ne doit pas convertir des mauvais montants', function() {
